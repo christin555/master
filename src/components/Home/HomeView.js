6e3b92adb385d73d144be11bc2204a67bdbde120
s.s.shaikhalov <s.s.shaykhalov@tmn3.etagi.com>
2021-09-20 01:50:31 +0500

    -Правка стилей мобильного меню -Правка цветов и ссылок футера -Убраны important стили в header -Анимация при переключении header'а при скролле -Цвета поиска в header -Ссылка в header -Правка заголовков -Правка блоков на главной -Добавлены стили для h2 -Правка placeholder у textField

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -2,29 +2,25 @@ import React from 'react';
 import s from './Home.module.scss';
 import Blocks from './Blocks';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className={s.contentContainer}>
-        <div className={s.wrapper}>
-          <div className={s.gradientOverlay}>
-            <div className={s.shadow} />
-            <div className={s.mediaBlock}>
-              <div className={s.text}>
-                                    РЕАЛИЗУЙТЕ МЕЧТЫ ВМЕСТЕ С НАМИ
-                <div className={s.slogan}>
-                  <span>проффесионализм</span>
-                  <span>ответственность</span>
-                  <span>надежность</span>
-                </div>
-              </div>
+const Home = () => (
+  <div className={s.contentContainer}>
+    <div className={s.wrapper}>
+      <div className={s.gradientOverlay}>
+        <div className={s.shadow} />
+        <div className={s.mediaBlock}>
+          <div className={s.text}>
+                                РЕАЛИЗУЙТЕ МЕЧТЫ ВМЕСТЕ С НАМИ
+            <div className={s.slogan}>
+              <span>проффесионализм</span>
+              <span>ответственность</span>
+              <span>надежность</span>
             </div>
           </div>
         </div>
-        <Blocks />
       </div>
-    );
-  }
-}
+    </div>
+    <Blocks />
+  </div>
+);
 
 export default Home;
