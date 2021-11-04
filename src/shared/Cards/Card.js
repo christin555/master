86e1572b08c2abea94631a19bbb073795e826445
Крис <kriska_555@mail.ru>
2021-11-04 20:24:58 +0500

    *

--- src/shared/Cards/Card.js
+++ src/shared/Cards/Card.js
@@ -73,11 +73,11 @@ class CardView extends React.Component {
             <Buttons {...this.props} />
           </CardMedia>
 
-          <Link to={pathname}>
+          <Link to={pathname} className = {s.contentContainer}>
             <CardContent
               className={s.content}
             >
-              {
+              <div className={s.header}>  {
                 brand && (
                   <span className={s.brand}>
                     {brand}
@@ -89,11 +89,10 @@ class CardView extends React.Component {
               <span className={s.name}>
                 {name}
               </span>
+              </div>
               {
                 price && (
-                  <span className={s.price}> {formatPrice(price)}
-                    <span className={s.unit}> за 1 м2 </span>
-                  </span>
+                  <span className={s.price}> {formatPrice(price)}</span>
                 ) || null
               }
             </CardContent>
